c2fd8a668469a28e5017cb2d1d72e80c84fa7151
Doval Gomes Guzzo <doval.guzzo@totvs.com.br>
2020-07-06 15:26:32 -0300

    Inclusão da Operação de Adição

--- js/actions.js
+++ js/actions.js
@@ -1,10 +1,14 @@
 
 var display = document.getElementById('display');
+var result_display = document.getElementById('result_display');
 var numbers = document.getElementsByClassName('number');
 var reset = document.getElementById('reset');
 
+var position_operation = 0;
+var operation = '';
+
 // Buttons Functions
-function addClickNumber() {
+function numeric_click() {
     for (var i = 0; i < numbers.length; i++) {
         numbers[i].addEventListener("click", function () {
             showDisplayValue(this.value);
@@ -12,7 +16,7 @@ function addClickNumber() {
     }
 }
 
-function acResetClick() {
+function reset_click() {
     reset.addEventListener("click", function () {
         display.value = '0';
     });
@@ -24,18 +28,80 @@ function showDisplayValue(value) {
         display.value = '';
     }
 
-    if (!isNaN(value)) {
-        display.value += value;
+    if (lastIsOperation()) {
+        position_operation = display.value.length - 1;
+        operation = display.value.substring(position_operation, position_operation + 1);
     }
+
+    display.value += value;
+
+    equal(operation);
+
 }
 
 // Operations Functions
 
+function sum_click() {
+    document.getElementById('sum').addEventListener("click", function () {
+        sum();
+    });
+}
+
+function equal_click() {
+    document.getElementById('equal').addEventListener("click", function () {
+        equal(operation, true);
+    })
+}
+
+function sum() {
+    if (!lastIsOperation()) {
+        display.value += '+';
+    }
+}
+
+function equal(operation, resume) {
+    if (operation) {
+        const display_operation = display.value.split(operation);
+
+        switch (operation) {
+            case '+':
+                var sum = 0;
+                display_operation.map(value => {
+                    sum += parseFloat(value);
+                });
+                result_display.value = sum;
+                break;
+            default:
+                break;
+        }
+        resumeDisplay(display_operation);
+    }
+
+    if (resume) {
+        display.value = result_display.value;
+    }
+}
+
+function resumeDisplay(display_operation) {
+    if (display_operation.length % 5 === 0) {
+        display.value = result_display.value;
+    }
+}
+
+function lastIsOperation() {
+    const display_trim = display.value.trim();
+    const display_length = display_trim.length;
+    const lastCharacter = display_trim.substring(display_length - 1);
+    return lastCharacter.indexOf('+') !== -1 || lastCharacter.indexOf('-') !== -1 || lastCharacter.indexOf('x') !== -1 || lastCharacter.indexOf('÷') !== -1
+}
+
 // DOM Functions
-function setEvents() {
-    addClickNumber();
-    acResetClick();
+function bind_events() {
+    numeric_click();
+    reset_click();
+    sum_click();
+    equal_click();
 }
 
 // Start Functions
-setEvents();
+bind_events();
